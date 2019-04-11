3fb1afa5c0be99f93eaac69fa5d357954f29b13f
Abdelfattah Mohamed <abdelftahmohamed30@gmail.com>
2019-04-11 23:11:31 +0200

    fix Quick Sort

--- src/eg/edu/alexu/csd/filestructure/sort/cs_23_cs24/MySort.java
+++ src/eg/edu/alexu/csd/filestructure/sort/cs_23_cs24/MySort.java
@@ -14,11 +14,11 @@ public class MySort<T extends Comparable<T>> implements ISort<T> {
 	public IHeap heapSort(ArrayList<T> unordered) {
 		// TODO Auto-generated method stub
 		IHeap<T> heap = new MyHeap<T>();
-		//INode<T> newNode = new MyNode<T>();
+		// INode<T> newNode = new MyNode<T>();
 		heap.build(unordered);
 		int size = unordered.size();
 		for (int i = 1; i < unordered.size(); i++) {
-			((MyHeap<T>)heap).swap(0, size - i);
+			((MyHeap<T>) heap).swap(0, size - i);
 			((MyHeap<T>) heap).heapifySize(size - i, heap.getRoot());
 		}
 		return heap;
@@ -39,35 +39,35 @@ public class MySort<T extends Comparable<T>> implements ISort<T> {
 		}
 	}
 
-	
-
 	@Override
 	public void sortFast(ArrayList<T> unordered) {
-		quickSort(unordered,0,unordered.size()-1);
-		
+		quickSort(unordered, 0, unordered.size() - 1);
 	}
-	
-	private void quickSort(ArrayList<T> unordered,int left , int right) {
-		if(left < right) {
-		int q = raondomized_partition(unordered,left,right);
-		quickSort(unordered,left,q-1);
-		quickSort(unordered,q+1,right);
+
+	private void quickSort(ArrayList<T> unordered, int left, int right) {
+		if (left < right) {
+			int q = raondomized_partition(unordered, left, right);
+			T value = unordered.get(q);
+			unordered.set(q, unordered.get(right));
+			unordered.set(right, value);
+			quickSort(unordered, left, q - 1);
+			quickSort(unordered, q + 1, right);
+		}
 	}
-	}
-	
-	private int raondomized_partition(ArrayList<T> unordered,int left , int right) {
-		int random = (int) (Math.random()*(right-left+1)+left); 
+
+	private int raondomized_partition(ArrayList<T> unordered, int left, int right) {
+		int random = (int) (Math.random() * (right - left + 1) + left);
 		T value = unordered.get(right);
 		unordered.set(right, unordered.get(random));
 		unordered.set(random, value);
-		return partition(unordered,left,right);
+		return partition(unordered, left, right);
 	}
-	
-	private int partition(ArrayList<T> unordered,int left , int right) {
+
+	private int partition(ArrayList<T> unordered, int left, int right) {
 		T elemnt = unordered.get(right);
 		int pointer = left;
-		for(int counter=left;counter<right;counter++) {
-			if(unordered.get(counter).compareTo(elemnt) < 0 ) {
+		for (int counter = left; counter < right; counter++) {
+			if (unordered.get(counter).compareTo(elemnt) < 0) {
 				T value = unordered.get(counter);
 				unordered.set(counter, unordered.get(pointer));
 				unordered.set(pointer, value);
